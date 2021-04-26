460a0a766892b4b82fc50b7167ae5113cdfc4234
RuslanPr0g <theruslan.prog@gmail.com>
2021-04-26 19:27:07 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IdealSleepTimeTeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
